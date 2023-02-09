1103465697ff44ba2be7971af4b5cdf6054f7a91
jimmymadrigalGL <jimmy.madrigal@gorillalogic.com>
2023-02-09 10:24:42 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-computed-deps
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-wqedkz)
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    Counts: <br />
+    &nbsp;&nbsp;- count1 = {{ count1 }} <button @click="count1++">Add 1</button
+    ><br />
+    &nbsp;&nbsp;- count2 = {{ count2 }} <button @click="count2++">Add 2</button
+    ><br />
+    &nbsp;&nbsp;- count3 = {{ count3 }} (not in calc)
+    <button @click="count3++">Add 3</button><br />
+    &nbsp;&nbsp;- count4 = {{ count4 }} <b>(not a ref!) </b>
+    <button @click="count4++">Add 4</button>
+  </div>
+  <br />
+  <div>Calculated: count 1 + count2 + count4 = {{ calc }}</div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+const count1 = ref(0);
+const count2 = ref(0);
+const count3 = ref(0);
+let count4 = 0;
+
+const calc = computed(() => {
+  console.log(
+    'calculated!',
+    count1.value + count2.value,
+    '=',
+    count1.value,
+    '+',
+    count2.value,
+    '+',
+    count4
+  );
+  return count1.value + count2.value + count4;
+});
+</script>
