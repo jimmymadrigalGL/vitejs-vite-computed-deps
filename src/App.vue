2d12635454ea6e3713ef3b4e01c2bb5d2f4126f6
jimmymadrigalGL <jimmy.madrigal@gorillalogic.com>
2023-02-16 09:04:33 -0600

    Conditional ref

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,13 @@
     &nbsp;&nbsp;- count3 = {{ count3 }} (not in calc)
     <button @click="count3++">Add 3</button><br />
     &nbsp;&nbsp;- count4 = {{ count4 }} <b>(not a ref!) </b>
-    <button @click="count4++">Add 4</button>
+    <button @click="count4++">Add 4</button><br />
+    &nbsp;&nbsp;- mult = {{ mult }}
+    <input type="checkbox" value="{mult}" @click="mult = !mult" /><br />
+    &nbsp;&nbsp;- by = {{ by }} <button @click="by++">Add By</button><br />
   </div>
   <br />
-  <div>Calculated: count 1 + count2 + count4 = {{ calc }}</div>
+  <div>Calculated: mult-by * (count 1 + count2 + count4 ) = {{ calc }}</div>
   <button @click="reset">Reest</button>
 </template>
 
@@ -23,12 +26,18 @@ const count1 = ref(0);
 const count2 = ref(0);
 const count3 = ref(0);
 let count4 = 0;
-const memoCalc = useMemoize((a, b, c, d) => {
-  console.log('calculated!', a + b + c, '=', a, '+', b, '+', c);
-  return a + b + c;
+const mult = ref(false);
+const by = ref(1);
+
+const memoCalc = useMemoize((a, b, c, e) => {
+  const result = e * (a + b + c);
+  console.log('calculated!', result, '=', e, '* (', a, '+', b, '+', c, ')');
+  return result;
 });
 
-const calc = computed(() => memoCalc(count1.value, count2.value, count4));
+const calc = computed(() =>
+  memoCalc(count1.value, count2.value, count4, mult.value ? by.value : 1)
+);
 
 const reset = () => {
   count1.value = 0;
